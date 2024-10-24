54c26bdb9b596b33c4e35545d4b48e2bdf5542c5
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-10-24 22:22:29 +0630

    [Finish] DapperService(Custom)

---
+++ YLPDotNetCore.RestApi/Controllers/BlogDapper2Controller.cs
@@ -0,0 +1,153 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using YLPDotNetCore.RestApi.Models;
+using YLPDotNetCore.Shared;
+
+namespace YLPDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogDapper2Controller : ControllerBase
+    {
+        private readonly DapperService _dapperService = new DapperService(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        [HttpGet]
+        public IActionResult GetBlog()
+        {
+            string query = "SELECT * FROM Tbl_Blog";
+            //using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            //List<BlogModel> lst = db.Query<BlogModel>(query).ToList();
+            var lst = _dapperService.Query<BlogModel>(query);
+            return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult EditBlog(int id)
+        {
+            var item = FindById(id);
+
+            if(item is null)
+            {
+                return NotFound("No Data Found.");
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogModel blog)
+        {
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent)";
+
+            //using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            //int result = db.Execute(query, blog);
+
+            int result = _dapperService.Execute(query, blog);
+
+            string message = result > 0 ? "Create Success." : "Creating Fail.";
+            return Ok(message);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id, BlogModel blog)
+        {
+            var item = FindById(id);
+            if(item is null)
+            {
+                return NotFound("No data found.");
+            }
+            blog.BlogId = id;
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+                SET [BlogTitle] = @BlogTitle
+                  ,[BlogAuthor] = @BlogAuthor
+                  ,[BlogContent] = @BlogContent
+                WHERE BlogId = @BlogId";
+            //using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            //int result = db.Execute(query, blog);
+
+            int result = _dapperService.Execute(query, blog);
+            string message = result > 0 ? "Update success." : "Updating fail.";
+            return Ok(message);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult PatchBlog(int id, BlogModel blog)
+        {
+            var item = FindById(id);
+            if (item is null)
+            {
+                return NotFound("No data found.");
+            }
+            string conditions = string.Empty;
+            if (!string.IsNullOrEmpty(blog.BlogTitle))
+            {
+                conditions += "[BlogTitle] = @BlogTitle, ";
+            }
+            if (!string.IsNullOrEmpty(blog.BlogAuthor))
+            {
+                conditions += "[BlogAuthor] = @BlogAuthor, ";
+            }
+            if (!string.IsNullOrEmpty(blog.BlogContent))
+            {
+                conditions += "[BlogContent] = @BlogContent, ";
+            }
+
+            if(conditions.Length == 0)
+            {
+                return NotFound("No Data to update.");
+            }
+
+            conditions = conditions.Substring(0, conditions.Length - 2);
+            blog.BlogId = id;
+
+            string query = $@"UPDATE [dbo].[Tbl_Blog]
+                SET {conditions}
+                WHERE BlogId = @BlogId";
+
+            //using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            //int result = db.Execute(query, blog);
+
+            int result = _dapperService.Execute(query, blog);
+            string message = result > 0 ? "Update success." : "Updating fail.";
+            return Ok(message);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+            var item = FindById(id);
+
+            if (item is null)
+            {
+                return NotFound("No Data Found.");
+            }
+            string query = "DELETE FROM [dbo].[Tbl_Blog] WHERE BlogId = @BlogId";
+
+            //using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            //int result = db.Execute(query, new BlogModel { BlogId = id});
+
+            int result = _dapperService.Execute(query, new BlogModel { BlogId = id});
+
+            string message = result > 0 ? "Delete Success." : "Deleting Fail.";
+            return Ok(message);
+        }
+
+        private BlogModel? FindById(int id)
+        {
+            string query = "SELECT * FROM Tbl_Blog WHERE BlogId = @BlogId";
+            //using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            //var item = db.Query<BlogModel>(query, new BlogModel { BlogId = id }).FirstOrDefault();
+            var item = _dapperService.QueryFirstOrDefault<BlogModel>(query, new BlogModel { BlogId = id });
+
+            return item;
+        }
+    }
+}
