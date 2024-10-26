dddbd238a9167c89b680fd844151f39fc831fcaf
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-10-27 00:36:12 +0630

    [Finish] Writing API from Json Data

---
+++ YLPDotNetCore.RestApiWithNLayer/Features/LatHtaukBayDin/LatHtaukBayDinController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace YLPDotNetCore.RestApiWithNLayer.Features.LatHtaukBayDin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LatHtaukBayDinController : ControllerBase
+    {
+        private async Task<LatHtaukBayDin> GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("data.json");
+            var model = JsonConvert.DeserializeObject<LatHtaukBayDin>(jsonStr);
+            return model;
+        }
+
+        [HttpGet("questions")]
+        public async Task<IActionResult> QuestionsAsync()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.questions);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> NumberAsync()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.numberList);
+        }
+
+        [HttpGet("{questionNo}/{no}")]
+        public async Task<IActionResult> Answer(int questionNo, int no)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.answers.FirstOrDefault(x => x.questionNo == questionNo && x.answerNo == no));
+        }
+    }
+
+
+    public class LatHtaukBayDin
+    {
+        public Question[] questions { get; set; }
+        public Answer[] answers { get; set; }
+        public string[] numberList { get; set; }
+    }
+
+    public class Question
+    {
+        public int questionNo { get; set; }
+        public string questionName { get; set; }
+    }
+
+    public class Answer
+    {
+        public int questionNo { get; set; }
+        public int answerNo { get; set; }
+        public string answerResult { get; set; }
+    }
+
+}
