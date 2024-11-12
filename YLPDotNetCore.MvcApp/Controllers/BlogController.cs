5fc986867af7db54c010ee9fe9094577e822e905
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-12 22:20:11 +0630

    [Updated] MvcAppp CRUD

--- YLPDotNetCore.MvcApp/Controllers/BlogController.cs
+++ YLPDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -17,6 +17,7 @@ namespace YLPDotNetCore.MvcApp.Controllers
         public async Task<IActionResult> Index()
         {
             var lst = await _db.Blogs
+                .AsNoTracking()
                 .OrderByDescending(x => x.BlogId)
                 .ToListAsync();
             return View(lst);
@@ -53,7 +54,9 @@ namespace YLPDotNetCore.MvcApp.Controllers
         [ActionName("Update")]
         public async Task<IActionResult> BlogUpdate(int id, BlogModel blog)
         {
-            var item = await _db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+            var item = await _db.Blogs
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.BlogId == id);
             if(item is null)
             {
                 return Redirect("/Blog");
@@ -62,6 +65,7 @@ namespace YLPDotNetCore.MvcApp.Controllers
             item.BlogAuthor = blog.BlogAuthor;
             item.BlogContent = blog.BlogContent;
 
+            _db.Entry(item).State = EntityState.Modified;
             await _db.SaveChangesAsync();
             return Redirect("/Blog");
         }
