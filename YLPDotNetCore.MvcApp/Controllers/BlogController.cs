97f85905cb9faef0ed0314cd39b2b2486e8b53a3
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-12 22:06:23 +0630

    [Finish] MvcAppp CRUD

--- YLPDotNetCore.MvcApp/Controllers/BlogController.cs
+++ YLPDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using YLPDotNetCore.MvcApp.Db;
+using YLPDotNetCore.MvcApp.Models;
 
 namespace YLPDotNetCore.MvcApp.Controllers
 {
@@ -15,8 +16,69 @@ namespace YLPDotNetCore.MvcApp.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var lst = await _db.Blogs.ToListAsync();
+            var lst = await _db.Blogs
+                .OrderByDescending(x => x.BlogId)
+                .ToListAsync();
             return View(lst);
+        }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> BlogCreate(BlogModel blog)
+        {
+            await _db.Blogs.AddAsync(blog);
+            await _db.SaveChangesAsync();
+            return Redirect("/Blog");
+        }
+
+        [HttpGet]
+        [ActionName("Edit")]
+        public async Task<IActionResult> BlogEdit(int id)
+        {
+            var item = await _db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+            if(item is null)
+            {
+                return Redirect("/Blog");
+            }
+            return View("BlogEdit", item);
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public async Task<IActionResult> BlogUpdate(int id, BlogModel blog)
+        {
+            var item = await _db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+            if(item is null)
+            {
+                return Redirect("/Blog");
+            }
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+
+            await _db.SaveChangesAsync();
+            return Redirect("/Blog");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> BlogDelete(int id)
+        {
+            var item = await _db.Blogs.FirstOrDefaultAsync(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Redirect("/Blog");
+            }
+
+            _db.Blogs.Remove(item); 
+            await _db.SaveChangesAsync();
+            return Redirect("/Blog");
         }
     }
 }
