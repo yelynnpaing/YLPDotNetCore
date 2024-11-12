effc6f954ccbf5e300c7facf818051bb8d38c607
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-12 17:06:40 +0630

    [Add - Read] MvcAppp

---
+++ YLPDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YLPDotNetCore.MvcApp.Db;
+
+namespace YLPDotNetCore.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public BlogController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var lst = await _db.Blogs.ToListAsync();
+            return View(lst);
+        }
+    }
+}
---
+++ YLPDotNetCore.MvcApp/Db/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using YLPDotNetCore.MvcApp.Models;
+
+
+namespace YLPDotNetCore.MvcApp.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        //}
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
