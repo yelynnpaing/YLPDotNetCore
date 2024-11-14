f44bf9dc39123fcc238ba1ab724fb6763592e361
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-14 23:16:05 +0630

    [Finish] HighCharts

---
+++ YLPDotNetCore.MvcChart/Controllers/HighCharts.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YLPDotNetCore.MvcChart.Controllers
+{
+    public class HighCharts : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View("PieChart");
+        }
+    }
+}
