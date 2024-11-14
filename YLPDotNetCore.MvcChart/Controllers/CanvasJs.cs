cb3ad3fcbc10779909df3968ad0b811e7ffdc0e7
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-14 23:29:45 +0630

    [Finish] CanvasJs

---
+++ YLPDotNetCore.MvcChart/Controllers/CanvasJs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YLPDotNetCore.MvcChart.Controllers
+{
+    public class CanvasJs : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View("LineChart");
+        }
+    }
+}
