327d4a2f388f5bbcdaf871311896a70e3caa1ab1
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-14 18:44:52 +0630

    [Finish] ApexChart ( Line Chart )

--- YLPDotNetCore.MvcChart/Controllers/ApexChartController.cs
+++ YLPDotNetCore.MvcChart/Controllers/ApexChartController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 using YLPDotNetCore.MvcChart.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace YLPDotNetCore.MvcChart.Controllers
 {
@@ -12,5 +14,21 @@ namespace YLPDotNetCore.MvcChart.Controllers
             model.series = new List<int>() { 44, 55, 13, 43, 22 };
             return View(model);
         }
+
+        public IActionResult LineChart()
+        {
+           List<Series> seriesList = new List<Series>()
+           {
+               new Series("Series A", new List<double>{ 1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6 } ),
+               new Series("Series B", new List<double>{20, 29, 37, 36, 44, 45, 50, 58})
+           };
+
+            List<int> categories = new List<int>() { 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016 };
+
+            LineChartModel model = new LineChartModel(seriesList, categories);
+            return View(model);
+        }
     }
 }
+
+
---
+++ YLPDotNetCore.MvcChart/Models/LineChartModel.cs
@@ -0,0 +1,25 @@
+﻿namespace YLPDotNetCore.MvcChart.Models
+{
+    public class LineChartModel
+    {
+        public List<Series> SeriesList { get; set; }
+        public List<int> Categories { get; set; }   
+        public LineChartModel(List<Series> seriesList, List<int> categories) 
+        {
+            this.SeriesList = seriesList;
+            this.Categories = categories;
+        }
+
+    }
+
+    public class Series
+    {
+        public string name { get; set; }
+        public List<double> data { get; set; }
+        public Series(string name, List<double> data)
+        {
+            this.name = name;
+            this.data = data;
+        }
+    }
+}
