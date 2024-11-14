c6d9b3308027468e58ddeccfc76b0b60fa3e7567
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-14 22:52:48 +0630

    [Finish] ChartJs

---
+++ YLPDotNetCore.MvcChart/Controllers/ChartJsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YLPDotNetCore.MvcChart.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult BarChart()
+        {
+            return View("BarChart");
+        }
+
+        public IActionResult LineChart()
+        {
+            return View("LineChart");
+        }
+    }
+}
