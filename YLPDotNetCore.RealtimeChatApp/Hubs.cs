c067376e9f041241208ae7c8cbebdd9625d22132
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-15 22:01:02 +0630

    [Finish] RealtimeChatApp ( Signalr )

---
+++ YLPDotNetCore.RealtimeChatApp/Hubs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace YLPDotNetCore.RealtimeChatApp
+{
+    public class ChatHub : Hub
+    {
+        public async Task ServerReceiveMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ClientReceiveMessage", user, message);
+        }
+    }
+}
---
+++ YLPDotNetCore.RealtimeChatApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YLPDotNetCore.RealtimeChatApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
