088b0d7c49ae864eb9b2cc9291e7518df78df530
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-15 13:11:10 +0630

    [Finish] text logging for ConsoleApp, ASP.Net Core

---
+++ YLPDotNetCore.ConsoleAppLogging/Program.cs
@@ -0,0 +1,32 @@
+﻿using Serilog;
+using System.Runtime.InteropServices;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/YLPDotNetCore.ConsoleAppLogging.txt",
+        rollingInterval: RollingInterval.Hour,
+        rollOnFileSizeLimit: true)
+    .CreateLogger();
+
+Log.Debug("Hello");
+Log.Fatal("Hello World");
+Log.Information("Hello");
+
+try
+{
+    // Your program here...
+    const string name = "Serilog";
+    Log.Information("Hello, {Name}!", name);
+    throw new InvalidOperationException("Oops...");
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Unhandled exception");
+}
+finally
+{
+    await Log.CloseAndFlushAsync(); // ensure all logs written before app exits
+}
+
+
--- YLPDotNetCore.MvcChart/Controllers/CanvasJs.cs
+++ YLPDotNetCore.MvcChart/Controllers/CanvasJs.cs
@@ -4,8 +4,16 @@ namespace YLPDotNetCore.MvcChart.Controllers
 {
     public class CanvasJs : Controller
     {
+        private readonly ILogger<CanvasJs> _logger;
+
+        public CanvasJs(ILogger<CanvasJs> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult LineChart()
         {
+            _logger.LogInformation(" Canvasjs ... ");
             return View("LineChart");
         }
     }
