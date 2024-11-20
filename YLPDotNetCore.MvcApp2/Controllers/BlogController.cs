a19e5a285a628f68ac095544fd5797eaa81d47b8
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-20 22:12:32 +0630

    [Add] ASP.NET Core MVC Ajax

---
+++ YLPDotNetCore.MvcApp2/Controllers/BlogController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YLPDotNetCore.MvcApp2.Database;
+
+namespace YLPDotNetCore.MvcApp2.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public BlogController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        [ActionName("Index")]
+        public IActionResult BlogIndex()
+        {
+            List<BlogEntity> lst = _db.Blogs.AsNoTracking()
+                .OrderByDescending(x => x.BlogId)
+                .ToList();
+            return View("BlogIndex", lst);
+        }
+
+        [HttpGet]
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public IActionResult BlogSave(BlogEntity blog)
+        {
+            _db.Blogs.Add(blog);
+            var result = _db.SaveChanges();
+            string message = result > 0 ? "Create success." : "Create fail.";
+            return Json(new { Message = message, IsSuccess = result > 0 });
+        }
+    }
+}
---
+++ YLPDotNetCore.MvcApp2/Database/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace YLPDotNetCore.MvcApp2.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+           
+        }
+
+        public DbSet<BlogEntity> Blogs { get; set; }
+    }
+
+    [Table("Tbl_Blog")]
+    public class BlogEntity
+    {
+        [Key]
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+
+    }
+}
