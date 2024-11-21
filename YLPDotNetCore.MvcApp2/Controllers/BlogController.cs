e2ad0878bb91e54876c8e9e50c03982f9524f588
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-21 15:01:26 +0630

    [Finish] ASP.NET Core MVC Ajax

--- YLPDotNetCore.MvcApp2/Controllers/BlogController.cs
+++ YLPDotNetCore.MvcApp2/Controllers/BlogController.cs
@@ -27,7 +27,7 @@ namespace YLPDotNetCore.MvcApp2.Controllers
         [ActionName("Create")]
         public IActionResult BlogCreate()
         {
-            return View("Create");
+            return View("BlogCreate");
         }
 
         [HttpPost]
@@ -37,6 +37,54 @@ namespace YLPDotNetCore.MvcApp2.Controllers
             _db.Blogs.Add(blog);
             var result = _db.SaveChanges();
             string message = result > 0 ? "Create success." : "Create fail.";
+            return Json(new { Message = message, IsSuccess = result > 0 });
+        }
+
+        [HttpGet]
+        [ActionName("Edit")]
+        public IActionResult BlogEdit(int id)
+        {
+            var item = _db.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if(item is null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("BlogEdit", item);
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public IActionResult BlogUpdate(int id, BlogEntity blog)
+        {
+            var item = _db.Blogs.FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Json(new {Message= "No Data Found.",  IsSuccess = false} );
+            }
+
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+
+            _db.Entry(item).State = EntityState.Modified;
+            var result = _db.SaveChanges();
+            string message = result > 0 ? "Update success." : "Updating fail.";
+            return Json(new { Message = message, IsSuccess = result > 0 });
+        }
+
+        [HttpPost]
+        [ActionName("Delete")] 
+        public IActionResult BlogDelete(BlogEntity blog)
+        {
+            var item = _db.Blogs.FirstOrDefault( x => x.BlogId == blog.BlogId);
+            if(item is null)
+            {
+                return Json(new { Message = "No data Found.", IsSuccess = false });
+            }
+
+            _db.Entry(item).State = EntityState.Deleted;
+            var result = _db.SaveChanges();
+            string message = result > 0 ? "Update success." : "Updating fail.";
             return Json(new { Message = message, IsSuccess = result > 0 });
         }
     }
