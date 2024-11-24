aa1bf748b3721ac8dea6c2ce6fca38639e0afaea
J Theo <99105619+yelynnpaing@users.noreply.github.com>
2024-11-24 10:57:55 +0630

    feat : middleware app

---
+++ YLPDotNetCore.MiddlewareApp/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YLPDotNetCore.MiddlewareApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginModel reqModel)
+        {
+            HttpContext.Session.SetString("name" , reqModel.Username);
+            return Redirect("/home");
+        }
+    }
+
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ YLPDotNetCore.MiddlewareApp/Middlewares/SessionMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace YLPDotNetCore.MiddlewareApp.Middlewares
+{
+    public class SessionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public SessionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(context.Request.Path == "/" || context.Request.Path.ToString().ToLower() == "/Login".ToLower())
+                    goto result;
+
+                string name = context.Session.GetString("name")!;
+                if (name == null)
+                {
+                    context.Response.Redirect("/");
+                }
+
+            result:
+            // Call the next delegate/middleware in the pipeline.
+            await _next(context);
+        }
+    }
+}
